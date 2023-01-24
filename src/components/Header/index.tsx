9e4e8858a271b99b26f64f7f6491fd584d0e683c
srv013 <srv013@gmail.com>
2023-01-24 12:46:12 -0300

    dia 5

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import style from "./layout.css";
+// import style from "./header.css";
 
 export function HeaderApp() {
     return (
-        <div className={style["header-component__container"]}>
-            <a className={style["header-component__logo"]}>
+        <div >
+            <a >
                 <img src="../src/assets/logo.png" />
             </a>
-           <nav className={style["header-component-nav"]}>
+           <nav >
                 <ul>
                     <li>
                         <Link key={"login"} to="/login">
@@ -19,7 +19,7 @@ export function HeaderApp() {
                     <li>Reportes</li>
                 </ul>
             </nav>
-            <div className={style["menu"]}>
+            <div >
                 <div></div>
                 <div></div>
                 <div></div>
