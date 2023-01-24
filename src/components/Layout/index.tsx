e0067bb33240a2f893baadd258f21e84b2653029
srv013 <srv013@gmail.com>
2023-01-24 13:41:34 -0300

    dia 5

--- src/components/Layout/index.tsx
+++ src/components/Layout/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
-import { HeaderApp } from "../header/index";
+import { HeaderApp } from "../header";
 import { Footer } from "../footer";
 
 export function Layout() {
