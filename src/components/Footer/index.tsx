592ae290cf3da111c8838558cfb673c1d094af01
srv013 <srv013@gmail.com>
2023-01-24 17:57:23 -0300

    dia 5-4

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,11 +1,11 @@
 import React from "react"
-import style from "./footer.css";
+import css from "./footer.css"
 
 export function Footer(){
 return (
-    <div className={style["footer-component__container"]}>
-      <img className={style["header-component__logo"]} src="../src/assets/logo.png" />
-        <div className={style["redes"]}>  
+    <div className={css["footer-component__container"]}>
+      <img className={css["header-component__logo"]} src="../src/assets/logo.png" />
+        <div className={css["redes"]}>  
                 <a >Instagram</a>
                 <a >Facebook</a>
                 <a >Github</a>
