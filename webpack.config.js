b3b8b9745f5d900d99204d768a2b95917599d52e
srv013 <srv013@gmail.com>
2023-01-24 17:05:12 -0300

    dia 5-b

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,35 @@
 const path = require("path");
 const dev = process.env.NODE_ENV == "development";
 const liveServer = require("live-server");
-
 if (dev) {
-  liveServer.start({
-    root: "./",
-    file: "src/index.html",
-  });
+    liveServer.start({
+        root: "./",
+        file: "src/index.html",
+    });
 }
-
 module.exports = {
-  watch: dev,
-  entry: "./src/index.tsx",
-  module: {
-    rules: [
-      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
-      {
-        test: /\.css$/i,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: {
-              modules: true,
+    watch: dev,
+    entry: "./src/index.tsx",
+    module: {
+        rules: [
+            { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
+            {
+                test: /\.css$/i,
+                use: [
+                    "style-loader",
+                    {
+                        loader: "css-loader",
+                        options: {
+                            modules: true,
+                        },
+                    },
+                ],
             },
-          },
         ],
-      },
-      {
-        test: /\.(png|jpg|gif)$/i,
-        use: [
-          { loader: "file-loader", options: { name: "assets/[hash].[ext]" } },
-        ],
-      },
-    ],
-  },
-  resolve: { extensions: [".tsx", ".js", ".ts", ".css"] },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-  },
+    },
+    resolve: { extensions: [".tsx", ".js", ".ts", ".css"] },
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        filename: "bundle.js",
+    },
 };
