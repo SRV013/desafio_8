f1de441416b2f12408857a37d59cbab6c25e935b
srv013 <srv013@gmail.com>
2023-01-24 16:27:16 -0300

    dia 5-b

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,43 @@
 const path = require("path");
 const dev = process.env.NODE_ENV == "development";
 const liveServer = require("live-server");
-const TsconfigPathsPlugin = require("tsconfig-paths-webpack-plugin");
-const customConfig = {
-  stats: {
-     errorDetails: true,
-     children: true
-  },
-}
+
 if (dev) {
   liveServer.start({
     root: "./",
-    file: "index.html",
+    file: "src/index.html",
   });
 }
 
 module.exports = {
-  entry: "./src/index.tsx",
   watch: dev,
+  entry: "./src/index.tsx",
   module: {
     rules: [
-      {
-        test: /\.tsx?$/,
-        use: "ts-loader",
-        exclude: /node_modules/,
-      },
+      { test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ },
       {
         test: /\.css$/i,
-        use: ["style-loader", {
-          loader: "css-loader",
-          options:{
-            modules: true,
-          }
-        }],
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|gif)$/i,
+        use: [
+          { loader: "file-loader", options: { name: "assets/[hash].[ext]" } },
+        ],
       },
     ],
   },
-  resolve: {
-    extensions: [".tsx", ".js", ".ts", ".css" ],
-    plugins: [
-      new TsconfigPathsPlugin({}),
-    ],
-  },
+  resolve: { extensions: [".tsx", ".js", ".ts", ".css"] },
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "bundle.js",
   },
-  
 };
