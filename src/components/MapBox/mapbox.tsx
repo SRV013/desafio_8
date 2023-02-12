d512a92b43557086091e94b22be3edaa9946139f
srv013 <srv013@gmail.com>
2023-02-11 21:03:57 -0300

    dia 14

--- src/components/MapBox/mapbox.tsx
+++ src/components/MapBox/mapbox.tsx
@@ -1,13 +1,14 @@
-import React, { useState } from "react";
+import React, { useRef, useEffect, useState } from "react";
 //import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 import { myLocation } from "hooks/index";
-import mapboxgl from 'mapbox-gl';
+import mapboxgl from "mapbox-gl";
 
 import { InputMap } from "ui/input_text";
 import styles from "./index.css";
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3J2MDEzIiwiYSI6ImNsYXJlOXQ3aTFqdnQzd21xNGxmNDJlYnkifQ.LBfdUDCBiV3fi1eKByUlFQ';
+mapboxgl.accessToken =
+    "pk.eyJ1Ijoic3J2MDEzIiwiYSI6ImNsYXJlOXQ3aTFqdnQzd21xNGxmNDJlYnkifQ.LBfdUDCBiV3fi1eKByUlFQ";
 
 // const Map = ReactMapboxGl({
 //     accessToken:
@@ -18,6 +19,29 @@ export function MapboxSeach(e) {
     const initialCoords: any = [location.lat, location.lng];
     const [coords, setCoords] = useState(initialCoords);
 
+    const mapContainer = useRef(null);
+    const map = useRef(null);
+    const [lng, setLng] = useState(location.lat);
+    const [lat, setLat] = useState(location.lng);
+    const [zoom, setZoom] = useState(9);
+
+    useEffect(() => {
+        if (map.current) return; // initialize map only once
+        map.current = new mapboxgl.Map({
+            container: mapContainer.current,
+            style: "mapbox://styles/mapbox/streets-v12",
+            center: [lng, lat],
+            zoom: zoom,
+        });
+    });
+    useEffect(() => {
+        if (!map.current) return; // wait for map to initialize
+        map.current.on('move', () => {
+        setLng(map.current.getCenter().lng.toFixed(4));
+        setLat(map.current.getCenter().lat.toFixed(4));
+        setZoom(map.current.getZoom().toFixed(2));
+        });
+        });
     async function buscar(e) {
         e.preventDefault();
         const buscar = e.target.ubicacion.value;
@@ -33,7 +57,17 @@ export function MapboxSeach(e) {
         <div className={styles["__container"]}>
             <div className={styles["header"]}>Mapa de localización</div>
             <form onSubmit={buscar}>
-                <Map
+                <div>
+                    <div className={styles["sidebar"]}>
+                        Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+                    </div>
+                    <div
+                        ref={mapContainer}
+                        className={styles["map-container"]}
+                    />
+                </div>
+
+                {/* <Map
                     style="mapbox://styles/mapbox/streets-v9"
                     containerStyle={{
                         height: "300px",
@@ -41,13 +75,13 @@ export function MapboxSeach(e) {
                     }}
                     zoom={[14]}
                     center={coords}
-                    movingMethod="flyTo">
-                    <InputMap
-                        type={"text"}
-                        name={"ubicacion"}
-                        placeholder="ej: La Rioja 1902, Mar del Plata"
-                    />
-                    {/* <Layer
+                    movingMethod="flyTo"> */}
+                <InputMap
+                    type={"text"}
+                    name={"ubicacion"}
+                    placeholder="ej: La Rioja 1902, Mar del Plata"
+                />
+                {/* <Layer
                         type="symbol"
                         id="marker"
                         layout={{
@@ -56,9 +90,11 @@ export function MapboxSeach(e) {
                         }}>
                         <Feature coordinates={coords} />
                     </Layer> */}
-                </Map>
+                {/* </Map> */}
             </form>
-            <div className={styles["footer"]}>ingrese su ubicacion y luego haga click afuera de la ventana !!!</div>
+            <div className={styles["footer"]}>
+                ingrese su ubicacion y luego haga click afuera de la ventana !!!
+            </div>
         </div>
     );
 }
